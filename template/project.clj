56b13edb98e74a2c66fc7cb62cbf59cae257e67d
Mike Piech <mpiech@alum.mit.edu>
2015-12-29 15:59:30 -0800

    fixed .0

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,5 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
     :plugins [[lein-ring "0.9.7"]]
-    :dependencies [[org.clojure/clojure "1.7"]
+    :dependencies [[org.clojure/clojure "1.7.0"]
                    [http-kit "2.1.19"]]
     :main webapp)
