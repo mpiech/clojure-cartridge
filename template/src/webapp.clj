5b3cf4fd8e5e30349c128947a8a47e1d36849a7f
Mike Piech <mpiech@alum.mit.edu>
2015-12-29 15:35:49 -0800

    First commit

---
+++ template/src/webapp.clj
@@ -0,0 +1,15 @@
+(ns webapp
+  (:use org.httpkit.server))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Clojure on OpenShift!"})
+
+(defn -main [& args]
+  (let [port (Integer/parseInt 
+              (get (System/getenv) 
+                   "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))
+        ip (get (System/getenv) 
+                "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")]
+    (run-server app {:ip ip :port port})))
